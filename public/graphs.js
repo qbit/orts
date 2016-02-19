72151c7af7808a0996325e4db03876258bed7283
Aaron Bieber <aaron@bolddaemon.com>
2016-02-18 19:15:40 -0700

    fix status / fail graph labels

--- public/graphs.js
+++ public/graphs.js
@@ -1,7 +1,8 @@
 function totalCounts(a) {
     var counts = {}, i, l, ret = [];
     for (i = 0, l = a.length; i < l; i++) {
-	var name = a[i].innerText;
+	var name = a[i].innerText || a[i].getAttribute('data-status');
+	console.log(name, a[i]);
 	if (! counts[name]) {
 	    counts[name] = 1;
 	} else {
@@ -16,73 +17,73 @@ function totalCounts(a) {
 }
 function countsByArch() {
     var a = document.getElementsByClassName("arch");
-    return totalCounts(a);
-}
+	return totalCounts(a);
+    }
 
-function countsByMedia() {
-    var a = document.getElementsByClassName("boot-media");
-    return totalCounts(a);
-}
+    function countsByMedia() {
+	var a = document.getElementsByClassName("boot-media");
+	return totalCounts(a);
+    }
 
-function countsByInstType() {
-    var a = document.getElementsByClassName("install-method");
-    return totalCounts(a);
-}
+    function countsByInstType() {
+	var a = document.getElementsByClassName("install-method");
+	return totalCounts(a);
+    }
 
-function countsByStatus() {
-    var a = document.getElementsByClassName("success-fail");
-    return totalCounts(a);
-}
+    function countsByStatus() {
+	var a = document.getElementsByClassName("success-fail");
+	return totalCounts(a);
+    }
 
-function chartDefault(name, agg) {
+    function chartDefault(name, agg) {
 
-    var defaultChart = {
-	chart: {
-	    plotBackgroundColor: null,
-	    plotBorderWidth: 0,
-	    plotShadow: false
-	},
-	title: {
-	    text: name, 
-	    align: 'center',
-	    verticalAlign: 'middle',
-	    y: 90
-	},
-	tooltip: {
-	    pointFormat: '<b>{point.percentage:.1f}%</b>'
-	},
-        credits: {
-            enabled: false
-        },
-	plotOptions: {
-	    pie: {
-		dataLabels: {
-		    enabled: true,
-		    distance: 20,
-		    style: {
-			fontWeight: 'bold',
-			color: 'black'
-		    }
-		},
-		startAngle: -90,
-		endAngle: 90,
-		center: ['50%', '75%']
-	    }
-	},
-	series: [{
-	    type: 'pie',
-	    name: name,
-	    innerSize: '50%',
-	    data: agg()
-	}]
-    };
+	var defaultChart = {
+	    chart: {
+		plotBackgroundColor: null,
+		plotBorderWidth: 0,
+		plotShadow: false
+	    },
+	    title: {
+		text: name, 
+		align: 'center',
+		verticalAlign: 'middle',
+		y: 90
+	    },
+	    tooltip: {
+		pointFormat: '<b>{point.percentage:.1f}%</b>'
+	    },
+            credits: {
+		enabled: false
+            },
+	    plotOptions: {
+		pie: {
+		    dataLabels: {
+			enabled: true,
+			distance: 20,
+			style: {
+			    fontWeight: 'bold',
+			    color: 'black'
+			}
+		    },
+		    startAngle: -90,
+		    endAngle: 90,
+		    center: ['50%', '75%']
+		}
+	    },
+	    series: [{
+		type: 'pie',
+		name: name,
+		innerSize: '50%',
+		data: agg()
+	    }]
+	};
 
-    return defaultChart;
-}
+	return defaultChart;
+    }
 
-$(function () {
-    $('#tests-by-arch').highcharts(chartDefault('Tests by arch', countsByArch));
-    $('#tests-by-media').highcharts(chartDefault('Tests by media', countsByMedia));
-    $('#tests-ins-vs-up').highcharts(chartDefault('Install vs Upgrade', countsByInstType));
-    $('#tests-suc-vs-fail').highcharts(chartDefault('Success vs Fail', countsByStatus));
-});
+    $(function () {
+	$('#tests-by-arch').highcharts(chartDefault('Tests by arch', countsByArch));
+	$('#tests-by-media').highcharts(chartDefault('Tests by media', countsByMedia));
+	$('#tests-ins-vs-up').highcharts(chartDefault('Install vs Upgrade', countsByInstType));
+	$('#tests-suc-vs-fail').highcharts(chartDefault('Success vs Fail', countsByStatus));
+    });
